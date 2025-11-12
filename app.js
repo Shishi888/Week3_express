f1929434f547284bb9a20af8f8c87d6f2985bc0d
Shirin <shirin.hdi8@gmail.com>
2025-11-12 22:22:29 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// Hello world route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Cat route
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: "Kitty",
+    birthdate: "2020-02-02",
+    weight: 4,
+    owner: "Liisa",
+    image: "https://loremflickr.com/320/240/cat"
+  };
+  res.json(cat);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
